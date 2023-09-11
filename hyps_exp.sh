7fb9debffcad6b1ea54f432af78020143397c65d
Srikesh <srikeshhp@gmail.com>
2023-09-12 00:38:26 +0530

    New bash code

--- hyps_exp.sh
+++ hyps_exp.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
-GPU_ID=0
-DATASET="IMDBBINARY"
+GPU_ID=1
 
 ALPHA_START_FACTOR=0.1 #Starting number in for loop.
 ALPHA_INC_FACTOR=0.1 # Increment factor
 MAX_ALPHA=1 #Modify it based on the number of experiments.
 
+MU1_START_FACTOR=0.5  #Starting number in for loop.
+MU1_INC_FACTOR=0.5 # Increment factor
+MAX_MU1=3 #Modify it based on the number of experiments.
 
-MU_START_FACTOR=0.5  #Starting number in for loop.
-MU_INC_FACTOR=0.5 # Increment factor
-MAX_MU=1 #Modify it based on the number of experiments.
+MU2_START_FACTOR=0.5  #Starting number in for loop.
+MU2_INC_FACTOR=0.5 # Increment factor
+MAX_MU2=3 #Modify it based on the number of experiments.
 
 for alpha in $(seq $ALPHA_START_FACTOR $ALPHA_INC_FACTOR $MAX_ALPHA);
 do
-  for mu in $(seq $MU_START_FACTOR $MU_INC_FACTOR $MAX_MU);
+  for mu1 in $(seq $MU1_START_FACTOR $MU1_INC_FACTOR $MAX_MU1);
   do
-      CUDA_VISIBLE_DEVICE=$GPU_ID python main.py --dataset $DATASET --K 205 --alpha "$alpha" --mu1 "$mu" --mu2 "$mu"
+    for mu2 in $(seq $MU2_START_FACTOR $MU2_INC_FACTOR $MAX_MU2);
+    do
+      CUDA_VISIBLE_DEVICE=$GPU_ID python main.py --dataset PTC --K 205 --alpha "$alpha" --mu1 "$mu1" --mu2 "$mu2"
+      CUDA_VISIBLE_DEVICE=$GPU_ID python main.py --dataset IMDBBINARY --K 205 --alpha "$alpha" --mu1 "$mu1" --mu2 "$mu2"
+    done
   done
 done
